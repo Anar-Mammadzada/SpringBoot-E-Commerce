338e5c2cd3b9b23539c39dfb97cacb0f3a7ef97b
Anar-Mammadzada <anarmammadza@gmail.com>
2023-02-27 17:32:49 +0400

    update

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
@@ -10,4 +10,9 @@ public class MainController {
     public String viewHomepage(){
         return "index";
     }
+
+    @GetMapping("/login")
+    public String viewLoginPage(){
+        return "login";
+    }
 }
